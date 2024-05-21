c02de336e80b21cdfd9d1446a12795407c19b84e
Md Sabbir Khan <mdsabbirkhan1972@gmail.com>
2024-05-22 02:21:35 +0600

    final

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,25 @@
 
+import { useContext } from "react";
 import Header from "../Header/Header";
+import { AuthContext } from "../../providers/AuthProvider";
 
 
 
 const Home = () => {
+    
+    const {loader}=useContext(AuthContext)
+    
+    if(loader){
+         return <div className="text-5xl text-center my-48">
+                <span className="loading loading-ring loading-xs"></span>
+                <span className="loading loading-ring loading-sm"></span>
+                <span className="loading loading-ring loading-md"></span>
+                <span className="loading  loading-infinity"></span>
+         </div>
+    }
     return (
         <div>
+            
             <Header></Header>      
         </div>
     );
